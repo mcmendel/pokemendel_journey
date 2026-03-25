d98e5314bf2bcceee5b7e59e65acc642c510d528
Nir Mendel <mcmendel@gmail.com>
2026-03-25 15:17:38 +0200

    add print pokemons page

--- app/routes/pokemon_routes.py
+++ app/routes/pokemon_routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, current_app
+from flask import Blueprint, render_template, current_app, abort
+
+from app.services.pokemon_service import get_marked_pokemon_entries
 
 pokemon_bp = Blueprint("pokemon", __name__)
 
@@ -7,3 +9,14 @@ pokemon_bp = Blueprint("pokemon", __name__)
 def index():
     games = current_app.config["GAMES"]
     return render_template("index.html", games=games)
+
+
+@pokemon_bp.route("/game/<game_id>/")
+def game_page(game_id):
+    games = current_app.config["GAMES"]
+    game = games.get(game_id)
+    if not game:
+        abort(404)
+
+    pokemons = get_marked_pokemon_entries(game.generation)
+    return render_template("game.html", games=games, game=game, game_id=game_id, pokemons=pokemons)
---
+++ app/services/pokemon_service.py
@@ -0,0 +1,7 @@
+from pokemendel_core.utils.evolutions import (
+    MarkedPokemonEntry,
+    get_marked_pokemon_entries,
+    is_eevee_family,
+)
+
+__all__ = ["MarkedPokemonEntry", "get_marked_pokemon_entries", "is_eevee_family"]
